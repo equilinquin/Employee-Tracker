dfa01e6f79db96fc6b0ed3f7fc49bf84104d1e4a
Elizabeth Garcia-Quilinquin <equilinquin@gmail.com>
2020-01-25 09:46:59 -0800

    corrected code in sql file

--- schema.sql
+++ schema.sql
@@ -26,9 +26,10 @@ CREATE TABLE crew (
     roles_id INT NOT NULL,
     captain_id INT,
     PRIMARY KEY (id),
-    CONSTRAINT fk_roles FOREIGN KEY (roles_id) REFERENCES roles(id),
-    CONSTRAINT fk_captain FOREIGN KEY (captain_id) REFERENCES employee(id)
+    CONSTRAINT fk_roles FOREIGN KEY (roles_id) REFERENCES roles(id)
+    
 );
+ALTER TABLE crew ADD CONSTRAINT fk_captain FOREIGN KEY (captain_id) REFERENCES crew(id);
 
 INSERT INTO pirates (piratename)
 VALUES ('Straw Hats'), 
@@ -36,40 +37,40 @@ VALUES ('Straw Hats'),
        ('Whitebeard'), 
        ('Big Mom'), 
        ('Red Hair'), 
-       ('Beast') 
+       ('Beast');
 
 INSERT INTO roles (title, bounty, pirates_id)
 VALUES ('Captain, Straw Hats', 500000000, 1), 
        ('Crew Member', 177000000, 1),
-       ('Captain, Blackbeard', 2247600000, 2),
+       ('Captain, Blackbeard', 247600000, 2),
        ('Crew Member', 20000000, 2),
-       ('Captain, Whitebeard', 5046000000, 3), 
+       ('Captain, Whitebeard', 546000000, 3), 
        ('Crew Member', 550000000, 3),
-       ('Captain, Big Mom', 4388000000, 4),
+       ('Captain, Big Mom', 488000000, 4),
        ('Crew Member', 700000000, 4),
-       ('Captain, Red Hair', 4048900000, 5),
+       ('Captain, Red Hair', 448900000, 5),
        ('Crew Member', 700000000, 5),
-       ('Captain, Beast', 4611100000, 6),
-       ('Crew Member', 1320000000, 6)
+       ('Captain, Beast', 461100000, 6),
+       ('Crew Member', 320000000, 6);
 
-INSERT INTO employee (first_name, last_name, roles_id, capitan_id)
+INSERT INTO crew (first_name, last_name, roles_id, captain_id)
 VALUES ('Luffy', 'Monkey', 1, NULL),
        ('Zoro', 'Roranoa', 2, 1),
        ('Sanji', 'Vinsmoke', 2, 1),
        ('Marshall', 'Teach', 3, NULL),
-       ('Burgess', 'Jesus', 4, 3),
-       ('Auger', 'Van', 4, 3),
+       ('Burgess', 'Jesus', 4, 4),
+       ('Auger', 'Van', 4, 4),
        ('Edward', 'Newgate', 5, NULL),
-       ('Ace', 'Portgas', 6, 5),
+       ('Ace', 'Portgas', 6, 7),
        ('Linlin', 'Charlotte', 7, NULL),
-       ('Katakuri', 'Charlotte', 8, 7),
-       ('Cracker', 'Charlotte', 8, 7),
-       ('Smoothie', 'Charlotte', 8, 7),
+       ('Katakuri', 'Charlotte', 8, 9),
+       ('Cracker', 'Charlotte', 8, 9),
+       ('Smoothie', 'Charlotte', 8, 9),
        ('Shanks', '', 9, NULL),
-       ('Benn', 'Beckman', 10, 9),
-       ('Lucky', 'Roux', 10, 9),
+       ('Benn', 'Beckman', 10, 13),
+       ('Lucky', 'Roux', 10, 13),
        ('Kaido', '', 11, NULL),
-       ('King', '', 12, 11),
-       ('Queen', '', 12, 11),
-       ('Jack', '', 12, 11)
+       ('King', '', 12, 16),
+       ('Queen', '', 12, 16),
+       ('Jack', '', 12, 16);
 
